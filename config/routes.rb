7acebda9d4130e432b8b93d5ddb27c043dd48eaa
tohi199210 <azucyan1939@gmail.com>
2020-10-26 23:20:39 +0900

    change 4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#top'
   
-  resources :posts, only: [:new, :create, :index, :show] do
+  resources :posts, only: [:new, :create, :index, :show, :edit, :destroy ,:update] do
   	resources :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
