82f1c460e0bce3a0702c00ba12eeeda701ea0013
tohi199210 <azucyan1939@gmail.com>
2020-11-06 23:39:56 +0900

    change 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
+  has_many :posts, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarks_posts, through: :bookmarks, source: :post
   
   attachment :image
 
